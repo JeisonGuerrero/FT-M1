c105569aa091b280aa75e7020ad0853b6266ceb4
JeisonGuerrero <jeison.g.s@hotmail.com>
2022-04-25 15:12:21 -0500

    "Homework 1"

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -2,12 +2,31 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-  // algo
+  var numeroDecimal = 0;
+var longNum = num.length;
+for (let i = 0; i < longNum; i++) {
+  let posicion = longNum - 1 - i;
+  let expo = 2 ** i;
+  numeroDecimal += num[posicion] * expo;
+}
+return numeroDecimal;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-}
+  var numeroBinaro = "";
+  while (num >= 1) {
+    numeroBinaro = (num % 2) + numeroBinaro;
+    num = Math.floor(num / 2);
+  } return numeroBinaro;
+};
+
+
+// No se pueden usar: 
+// parseInt
+// toString
+
+
 
 module.exports = {
   BinarioADecimal,
