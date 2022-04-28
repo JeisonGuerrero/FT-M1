8aa05f242cc56ecf8c04056c171bbcef1a782d06
JeisonGuerrero <jeison.g.s@hotmail.com>
2022-04-28 11:04:58 -0500

    Homework resuelto 03

--- 03-EstructuraDeDatos-I/homework/homework.js
+++ 03-EstructuraDeDatos-I/homework/homework.js
@@ -7,6 +7,10 @@ function nFactorial(n) {
   // devolvé el factorial de n (n!)
   // ej:
   // el factorial de 3 es 6 (3 * 2 * 1)
+  if (n === 1 || n === 0) return 1
+  if (n < 0) return 'No existe factorial de numeros negativos :D';
+  if (n % 1 !== 0) return 'Solo se reciben numeros enteros';
+  return n * nFactorial(n - 1)
 }
 
 function nFibonacci(n) {
@@ -15,8 +19,15 @@ function nFibonacci(n) {
   // nFibonacci(0) // 0  // el elemento 0 es cero
   // nFibonacci(1) // 1 // el elemento 1 es 1
   // nFibonacci(6) // 1 // el elemento 6 es 8
-
+  if (n == 1) {
+    return 1;
+  }
+  if (n == 0) {
+    return 0;
+  }
+  return nFibonacci(n - 1) + nFibonacci(n - 2)
 }
+
 
 // Para esta parte no es necesario utilizar recursión.
 // Implementa la clase Queue que debe contener los siguientes métodos:
@@ -25,9 +36,24 @@ function nFibonacci(n) {
 // size: Devuelve el número de elementos que contiene la queue.
 
 function Queue() {
-
+  this.arr = []
 }
 
+Queue.prototype.enqueue = function (n) {
+  this.arr.push(n)
+  return this
+}
+Queue.prototype.dequeue = function () {
+  return this.arr.shift()
+}
+Queue.prototype.size = function () {
+  return this.arr.length
+}
+//console.log(pila.enqueue(4))
+//console.log(pila.enqueue(5))
+//console.log(pila.enqueue(6))
+//console.log(pila.dequeue())
+//console.log(pila.size(6))
 // No modifiquen nada debajo de esta linea
 // --------------------------------
 
