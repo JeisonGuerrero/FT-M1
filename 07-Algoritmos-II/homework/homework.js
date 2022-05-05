a06b32e162aa9a2a8dd23d131be56da544f3fc65
JeisonGuerrero <jeison.g.s@hotmail.com>
2022-05-05 09:02:49 -0500

    Homework resuelto

--- 07-Algoritmos-II/homework/homework.js
+++ 07-Algoritmos-II/homework/homework.js
@@ -6,7 +6,19 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  if (array.length <= 1) return array
+  const pivot = array[Math.floor(Math.random() * array.length)]
+  let izq = []
+  let der = []
+  let mid = []
+  for (let value of array) {
+    if (value < pivot) {
+      izq.push(value)
+    }
+    else if (value > pivot) { der.push(value) }
+    else { mid.push(value) }
+  }
+  return quickSort(izq).concat(mid.concat(quickSort(der)))
 }
 
 function mergeSort(array) {
@@ -14,7 +26,21 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if (array.length === 1) return array
+  let mitad = Math.floor(array.length / 2);
+  let inicio = array.slice(0, mitad);
+  let final = array.slice(mitad);
+  return merge(mergeSort(inicio),mergeSort(final))
+}
+function merge(inicio, final) {
+  let arr = []
+  while (inicio.length && final.length) {
+    if (inicio[0] < final[0]) { arr.push(inicio.shift()) }
+    else {
+      arr.push(final.shift())
 
+    }
+  } return arr.concat(inicio.concat(final))
 }
 
 // No modificar nada debajo de esta línea
